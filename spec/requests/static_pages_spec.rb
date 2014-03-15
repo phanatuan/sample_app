b210dbfeb8432d73c6c8ddc1fcfc03d43f617c3c
Phan Anh Tuan <phananhtuan1011@gmail.com>
2014-03-15 13:32:17 +0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,56 +3,30 @@ require 'spec_helper'
 	
 describe "Static Pages" do 
 
-	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+	subject { page }
 
-	describe "Homepage" do 
-
-		it "should have the content 'Home' " do
-			visit '/static_pages/home'
-			expect(page).to have_content('Home') 
-		end
-
-		it "should have the base title" do 
-			visit '/static_pages/home'
-			expect(page).to have_title("#{base_title}")
-		end
-
-
+	describe "Homepage" do
+		before { visit root_path }
+		it { should have_content('Home') } 
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home') }
 	end
 
 	describe "Help Page" do 
-		it "should have the string 'Help' " do
-			visit '/static_pages/help' 
-			expect(page).to have_content('Help')
-		end
-
-		it "should have the right title Help" do 
-			visit '/static_pages/help'
-			expect(page).to have_title("#{base_title} | Help")
-		end
+		before { visit help_path}
+		it { should have_content('Help') }
+		it { should have_title(full_title('Help')) }  
 	end	
 
 	describe "About Page" do 
-		it "should have the content 'About' " do 
-			visit '/static_pages/about' 
-			expect(page).to have_content('About')
-		end
-		
-		it "should have the right title About" do
-			visit '/static_pages/about' 
-			expect(page).to have_title("#{base_title} | About")
-		end
+		before { visit about_path }
+		it { should have_content('About') } 
+		it { should have_title(full_title('About')) }
 	end
 
 	describe "Contact Page" do 
-		it "should have the content 'Contact'" do 
-			visit '/static_pages/contact' 
-			expect(page).to have_content('Contact')
-		end
-
-		it "should have the title 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_title("#{base_title} | Contact") 
-		end
+		before { visit contact_path }
+		it { should have_content('Contact') }
+		it { should have_title(full_title('Contact')) }
 	end
 end
