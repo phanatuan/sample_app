21049397f63b2ecc527ce21bb6b4ce01ea4e280a
Phan Anh Tuan <phananhtuan1011@gmail.com>
2014-03-16 14:40:17 +0700

    Resolve conflict with the branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140316024340) do
 
   create_table "users", force: true do |t|
@@ -20,16 +19,6 @@ ActiveRecord::Schema.define(version: 20140316024340) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-=======
-ActiveRecord::Schema.define(version: 20140315181616) do
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "password_digest"
->>>>>>> modeling-users
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
